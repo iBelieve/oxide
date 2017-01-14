cd64da7ece3561a3148bf2760d038f955a853e99
Michael Spencer <sonrisesoftware@gmail.com>
2017-01-13 20:59:43 -0600

    Ditch the x86 directory and go x86_64 only

--- kernel/src/arch/mod.rs
+++ kernel/src/arch/mod.rs
@@ -1,17 +1,6 @@
 // Export our platform-specific modules.
 
+mod x86_64;
+
 #[cfg(target_arch="x86_64")]
 pub use self::x86_64::{io, mem};
-
-#[cfg(target_arch="x86")]
-pub use self::x86_64::{io, mem};
-
-#[cfg(target_arch="x86_64")]
-pub mod x86_64;
-
-// This needs to be visible but not public on x86_64 (for things common to x86 and x86_64)
-#[cfg(target_arch="x86_64")]
-mod x86;
-
-#[cfg(target_arch="x86")]
-pub mod x86_64;
--- kernel/src/arch/x86_64/mod.rs
+++ kernel/src/arch/x86_64/mod.rs
@@ -1,2 +1,2 @@
+pub mod io;
 pub mod mem;
-pub use super::x86::io;
